15037a9a8536726496f19ac2d04c3c6c9273447f
kritsanasak kuntaros <roboconk@gmail.com>
2017-01-13 10:31:51 +0700

    just delete

--- applications/views/page/submenu.php
+++
@@ -1,9 +0,0 @@
-<ul>
-	<?php 
-	foreach ($items as $key => $item) {
-		?>
-		<li><a href="<?=getUrl();?>page/lists/<?=$depart_id;?>/<?=$item['id'];?>"><?=$item['title'];?></a></li>
-		<?php
-	}
-	?>
-</ul>
